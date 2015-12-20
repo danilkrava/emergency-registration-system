0eabf2bee2642347ea6169c016635eda6ec7f35d
Oleksandr <shoolex007@gmail.com>
2015-12-20 19:06:00 +0200

    Add test data

--- controller/MainController.java
+++ controller/MainController.java
@@ -1,13 +1,9 @@
 package controller;
 
-import model.AreaType;
-import model.Emergency;
-import model.Organisation;
-
-import model.SeverityType;
-
+import model.*;
 
 import java.util.ArrayList;
+import java.sql.Date;
 import java.util.List;
 
 /**
@@ -18,12 +14,17 @@ public class MainController {
     protected List<Organisation> organisations = new ArrayList<Organisation>();
     protected List<SeverityType> severities = new ArrayList<SeverityType>();
     private List<AreaType> areaTypes = new ArrayList<AreaType>();
+    private List<Region> regions = new ArrayList<Region>();
 
     public MainController() {
         for (int i = 0; i < 5; i++) {
             areaTypes.add(new AreaType(i, "type #" + i, i * 12.75));
             severities.add(new SeverityType(i, "Blya Fuck" + i));
+            regions.add(new Region(i, "Region #"+i));
+            organisations.add(new Organisation("Organisation #"+i, "Address #"+i, i, regions.get(i)));
+            emergencies.add(new Emergency(new Date(2000+i, i, i), i, areaTypes.get(i), severities.get(i), organisations.get(i)));
         }
+
     }
 
     public List<Emergency> getEmergencies() {
