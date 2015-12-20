4fefafb8c14ae40163d7fa917f352a0d8277d1b1
danilkrava <dani.kravchen@yandex.ru>
2015-12-20 21:31:29 +0200

    меню

--- view/AddOrganisationController.java
+++ view/AddOrganisationController.java
@@ -1,108 +1,64 @@
 package view;
 
+import dao.DaoFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.Organisation;
+import model.Region;
 
 /**
  * Created by Крава on 20.12.2015.
  */
-public class AddDialogController {
-/*
+public class AddOrganisationController {
     @FXML
-    private TextField firstNameField;
+    private TextField nameField;
     @FXML
-    private TextField lastNameField;
+    private TextField adressField;
     @FXML
-    private TextField streetField;
-    @FXML
-    private TextField postalCodeField;
-    @FXML
-    private TextField cityField;
-    @FXML
-    private TextField birthdayField;
-
+    private TextField regionField;
 
     private Stage dialogStage;
-    private Person person;
+    //  private Person person;
     private boolean okClicked = false;
 
-    *//**
-     * Initializes the controller class. This method is automatically called
-     * after the fxml file has been loaded.
-     *//*
+
     @FXML
     private void initialize() {
     }
 
-    *//**
-     * Sets the stage of this dialog.
-     *
-     * @param dialogStage
-     *//*
+
     public void setDialogStage(Stage dialogStage) {
         this.dialogStage = dialogStage;
     }
 
-    *//**
-     * Sets the person to be edited in the dialog.
-     *
-     * @param person
-     *//*
-    public void setPerson(Person person) {
-        this.person = person;
 
-        firstNameField.setText(person.getFirstName());
-        lastNameField.setText(person.getLastName());
-        streetField.setText(person.getStreet());
-        postalCodeField.setText(Integer.toString(person.getPostalCode()));
-        cityField.setText(person.getCity());
-        birthdayField.setText(DateUtil.format(person.getBirthday()));
-        birthdayField.setPromptText("dd.mm.yyyy");
-    }
 
-    *//**
-     * Returns true if the user clicked OK, false otherwise.
-     *
-     * @return
-     *//*
+
     public boolean isOkClicked() {
         return okClicked;
     }
 
-    *//**
-     * Called when the user clicks ok.
-     *//*
+
     @FXML
     private void handleOk() {
         if (isInputValid()) {
-            person.setFirstName(firstNameField.getText());
-            person.setLastName(lastNameField.getText());
-            person.setStreet(streetField.getText());
-            person.setPostalCode(Integer.parseInt(postalCodeField.getText()));
-            person.setCity(cityField.getText());
-            person.setBirthday(DateUtil.parse(birthdayField.getText()));
-
+            Organisation organisation = new Organisation(nameField.getText(), adressField.getText(), new Region(regionField.getText()));
+            DaoFactory.
             okClicked = true;
             dialogStage.close();
         }
     }
 
-    *//**
-     * Called when the user clicks cancel.
-     *//*
+
     @FXML
     private void handleCancel() {
         dialogStage.close();
     }
 
-    *//**
-     * Validates the user input in the text fields.
-     *
-     * @return true if the input is valid
-     *//*
+
     private boolean isInputValid() {
         String errorMessage = "";
 
@@ -153,5 +109,5 @@ public class AddDialogController {
 
             return false;
         }
-    }*/
+    }
 }
