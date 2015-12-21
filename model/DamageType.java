84e11b0c882875a30f39bb3ec6212810b631c51c
Oleksandr <shoolex007@gmail.com>
2015-12-21 02:13:57 +0200

    DAMAGE TYPE class added

---
+++ model/DamageType.java
@@ -0,0 +1,34 @@
+package model;
+
+/**
+ * Created by Oleksandr on 21.12.2015.
+ */
+public class DamageType {
+    private int id;
+    private String name;
+
+    public DamageType(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
