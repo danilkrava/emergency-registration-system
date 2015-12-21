a1036d53f6f729bee055fb742bff654e39854e02
Ilya Doroshenko <doroshenko.illia@gmail.com>
2015-12-21 02:56:21 +0200

    +PersonDao +DamageTypeDao

---
+++ dao/DamageTypeDao.java
@@ -0,0 +1,108 @@
+package dao;
+
+import model.DamageType;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ilya on 21.12.2015.
+ */
+public class DamageTypeDao {
+    private final Connection connection;
+
+    public DamageTypeDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    public DamageType get(int id) throws SQLException {
+        String sql = "SELECT * FROM damage_type WHERE damage_type = ?;";
+        PreparedStatement stm = connection.prepareStatement(sql);
+        stm.setInt(1, id);
+        ResultSet rs = stm.executeQuery();
+        rs.next();
+
+        DamageType a = new DamageType();
+        a.setId(rs.getInt("damage_type"));
+        a.setName(rs.getString("name"));
+        return a;
+    }
+
+    public List<DamageType> getAll() throws SQLException{
+        List<DamageType> list = new ArrayList<>();
+
+        String sql = "SELECT * FROM damage_type;";
+        PreparedStatement stm = connection.prepareStatement(sql);
+        ResultSet rs = stm.executeQuery();
+        while (rs.next()) {
+            DamageType a = new DamageType();
+            a.setId(rs.getInt("damage_type"));
+            a.setName(rs.getString("name"));
+            list.add(a);
+        }
+        return list;
+    }
+
+    public List<DamageType> getByPerson(int personId) throws  SQLException{
+        List<DamageType> list = new ArrayList<>();
+
+        String sql = "SELECT * FROM damage_type INNER JOIN damage_person_mapping as dpm on dpm.damage_type_id=damage_type.damage_type where dpm.person_id=?;";
+        PreparedStatement stm = connection.prepareStatement(sql);
+        stm.setInt(1, personId);
+        ResultSet rs = stm.executeQuery();
+        while (rs.next()) {
+            DamageType a = new DamageType();
+            a.setId(rs.getInt("damage_type"));
+            a.setName(rs.getString("name"));
+            list.add(a);
+        }
+        return list;
+    }
+
+    public void add(DamageType obj) throws SQLException{
+        String sql = "INSERT INTO damage_type (name) VALUES (?);";
+        try (PreparedStatement stm = connection.prepareStatement(sql)) {
+            stm.setString(1, obj.getName());
+            int count = stm.executeUpdate();
+            if (count != 1)
+                throw new SQLException(count + " records were modified instead of 1!");
+
+        }
+
+        sql = "SELECT MAX(damage_type) FROM damage_type;";
+        try (PreparedStatement stm = connection.prepareStatement(sql)) {
+            ResultSet rs = stm.executeQuery();
+            rs.next();
+            obj.setId(rs.getInt(1));
+
+        }
+    }
+
+    public void update(DamageType obj) throws SQLException {
+        String sql = "UPDATE damage_type SET name=? WHERE damage_type=?";
+        try (PreparedStatement stm = connection.prepareStatement(sql)) {
+            stm.setString(1, obj.getName());
+            stm.setInt(2, obj.getId());
+            int count = stm.executeUpdate();
+            if (count != 1) {
+                throw new SQLException(count + " records were modified instead of 1!");
+            }
+        }
+    }
+
+    public void delete(DamageType obj) throws SQLException {
+        String sql = "DELETE FROM damage_type WHERE damage_type = ?;";
+        try (PreparedStatement stm = connection.prepareStatement(sql)) {
+            stm.setInt(1, obj.getId());
+            int count = stm.executeUpdate();
+            if (count != 1) {
+                throw new SQLException(count + " records were modified instead of 1!");
+            }
+        }
+    }
+
+}
---
+++ dao/PersonDao.java
@@ -0,0 +1,102 @@
+package dao;
+
+import model.Person;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ilya on 21.12.2015.
+ */
+public class PersonDao {
+    private final Connection connection;
+
+    public PersonDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    public Person get(int id) throws SQLException {
+        String sql = "SELECT * FROM person WHERE person_id = ?;";
+        PreparedStatement stm = connection.prepareStatement(sql);
+        stm.setInt(1, id);
+        ResultSet rs = stm.executeQuery();
+        rs.next();
+
+        Person a = new Person();
+        a.setId(rs.getInt("person_id"));
+        a.setName(rs.getString("name"));
+        a.setSurname(rs.getString("last_name"));
+        a.setMiddleName(rs.getString("middle_name"));
+        a.setBirthDate(rs.getDate("birth_date"));
+        return a;
+    }
+
+    public List<Person> getByEmergency(int emergencyId) throws SQLException{
+        List<Person> list = new ArrayList<>();
+
+        String sql = "SELECT * FROM person INNER JOIN emergency_person_mapping as epm on epm.person_id=person.person_id WHERE emp.organisation_id=?;";
+        PreparedStatement stm = connection.prepareStatement(sql);
+        stm.setInt(1,emergencyId);
+        ResultSet rs = stm.executeQuery();
+        while (rs.next()) {
+            Person a = new Person();
+            a.setId(rs.getInt("person_id"));
+            a.setName(rs.getString("name"));
+            a.setSurname(rs.getString("last_name"));
+            a.setMiddleName(rs.getString("middle_name"));
+            a.setBirthDate(rs.getDate("birth_date"));
+            list.add(a);
+        }
+        return list;
+    }
+
+    public void add(Person obj) throws SQLException{
+        String sql = "INSERT INTO person (name, last_name, middle_name, birth_date) VALUES (?,?,?,?);";
+        try (PreparedStatement stm = connection.prepareStatement(sql)) {
+            stm.setString(1, obj.getName());
+            stm.setString(2, obj.getSurname());
+            stm.setString(3, obj.getMiddleName());
+            stm.setDate(4, obj.getBirthDate());
+            int count = stm.executeUpdate();
+            if (count != 1)
+                throw new SQLException(count + " records were modified instead of 1!");
+        }
+
+        sql = "SELECT MAX(person_id) FROM person;";
+        try (PreparedStatement stm = connection.prepareStatement(sql)) {
+            ResultSet rs = stm.executeQuery();
+            rs.next();
+            obj.setId(rs.getInt(1));
+        }
+    }
+
+    public void update(Person obj) throws SQLException {
+        String sql = "UPDATE person SET name=?, middle_name=?, last_name=?, birth_date=? WHERE person_id=?";
+        try (PreparedStatement stm = connection.prepareStatement(sql)) {
+            stm.setString(1, obj.getName());
+            stm.setString(2, obj.getMiddleName());
+            stm.setString(3, obj.getSurname());
+            stm.setDate(4, obj.getBirthDate());
+            stm.setInt(5, obj.getId());
+            int count = stm.executeUpdate();
+            if (count != 1) {
+                throw new SQLException(count + " records were modified instead of 1!");
+            }
+        }
+    }
+
+    public void delete(Person obj) throws SQLException {
+        String sql = "DELETE FROM person WHERE person_id = ?;";
+        try (PreparedStatement stm = connection.prepareStatement(sql)) {
+            stm.setInt(1, obj.getId());
+            int count = stm.executeUpdate();
+            if (count != 1) {
+                throw new SQLException(count + " records were modified instead of 1!");
+            }
+        }
+    }
+}
