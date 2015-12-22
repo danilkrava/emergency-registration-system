b0fcb10f39ba4b8b27841b103668547210e19fb7
Ilya Doroshenko <doroshenko.illia@gmail.com>
2015-12-22 18:06:42 +0200

    file output finished

--- view/DocCreator.java
+++ view/DocCreator.java
@@ -19,17 +19,18 @@ public class DocCreator {
         FileOutputStream fos = new FileOutputStream(file.getAbsolutePath());
         XWPFDocument doc = new XWPFDocument();
 
-        XWPFTable table = doc.createTable(list.size()+1, 4);
-        table.getRow(1).getCell(1).setText("id");
-        table.getRow(1).getCell(2).setText("Дата");
-        table.getRow(1).getCell(3).setText("Рівень радіації");
-        table.getRow(1).getCell(4).setText("Інформація");
+        XWPFTable table = doc.createTable(list.size() + 1, 4);
+        table.getRow(0).getCell(0).setText("id");
+        table.getRow(0).getCell(1).setText("Дата");
+        table.getRow(0).getCell(2).setText("Рівень радіації");
+        table.getRow(0).getCell(3).setText("Інформація");
 
-        for (int i = 0; i<list.size(); i++) {
-            table.getRow(i+2).getCell(1).setText(Integer.toString(list.get(i).getId()));
-            table.getRow(i+2).getCell(2).setText(list.get(i).getDate().toString());
-            table.getRow(i+2).getCell(3).setText(Double.toString(list.get(i).getRadiation()));
-            table.getRow(i+2).getCell(4).setText(list.get(i).getInfo());
+        table.setWidth(400);
+        for (int i = 0; i < list.size(); i++) {
+            table.getRow(i + 1).getCell(0).setText(Integer.toString(list.get(i).getId()));
+            table.getRow(i + 1).getCell(1).setText(list.get(i).getDate().toString());
+            table.getRow(i + 1).getCell(2).setText(Double.toString(list.get(i).getRadiation()));
+            table.getRow(i + 1).getCell(3).setText(list.get(i).getInfo());
         }
 
 
